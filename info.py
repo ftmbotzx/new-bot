bfd51adda06b616f7660877780ea2027eca3ebd3
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-08-14 10:37:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ FAILD_CHAT_ID = int(os.getenv("FAILD_CHAT_ID", "-1002590983675"))
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "media")
 PORT = int(os.getenv("PORT", "8080"))
 FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL", "-1002379643238"))
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Ansh089:Ansh089@cluster0.y8tpouc.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ftmbotzx:ftmbotzx@cluster0.0b8imks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 MONGO_NAME = os.getenv("MONGO_NAME", "SpotifyDL")
 ADMINS = [5660839376, 6167872503, 5961011848, 6538627123]
